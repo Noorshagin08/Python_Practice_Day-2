7b08abdf0a7a14757f0e88e17bf694f517992aff
Noorshagin08 <noorshagin05@gmail.com>
2025-06-12 22:58:37 +0530

    Arithmatic,Relational,Assignment,Logical and Bitwise Operators

---
+++ Operators.py
@@ -0,0 +1,58 @@
+#Arithmatic Operators
+a=100
+b=10
+print("add:",a+b)
+print("subtract:",a-b)
+print("multiply:",a*b)
+print("division:",a/b)
+print("floor division:",a//b)
+print("modulus:",a%b)
+print("exponential:",2**3)
+
+#Assignmrnt Operators
+a=125
+a+=5
+print(a)
+a-=5
+print(a)
+a*=5
+print(a)
+a/=5
+print(a)
+a//=5
+print(a)
+a%=5
+print(a)
+b=2
+b**=5
+print(b)
+
+#Comparision Operator
+a=10
+b=20
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>=b)
+
+#Logical Operator
+a=25
+print(a >= 25 and a <= 30)
+print(a <= 35 or a <= 10)
+print(not(a >= 25 and a <= 30))
+
+#Bitwise Operators
+a=10
+b=11
+print(a&b)
+print(a|b)
+print(a^b)
+print(~a)
+print(a<<2)
+print(a>>2)
+
+
+
+
+
+
